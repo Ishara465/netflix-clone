ba85320f2b579122ea286a9e2d2a23ed22cbc523
Ishara465 <116372693+Ishara465@users.noreply.github.com>
2025-03-01 20:18:21 +0530

    create frontend signUP,Logout and home page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
