06bda46d420b3daeeca803a4fbff7f6204f2f7e2
Ishara465 <116372693+Ishara465@users.noreply.github.com>
2025-03-04 13:56:14 +0530

    create watch page

---
+++ frontend/src/utils/dateFunction.js
@@ -0,0 +1,7 @@
+export function formatReleaseDate(date){
+    return new Date(date).toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    });
+}
